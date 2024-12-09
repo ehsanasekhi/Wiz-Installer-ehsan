96a4ec0d93c6a233b376ebf9b5cbbfa70873f3c5
ehsanasekhi <127648491+ehsanasekhi@users.noreply.github.com>
2024-12-09 21:43:17 +0330

    Update db.php

--- panel/includ/db.php
+++ panel/includ/db.php
@@ -1,6 +1,6 @@
 <?php
 include '../bot/baseInfo.php';
-$servername = "localhost";
+$servername = "mysql1.serv00.com";
 $conn = new mysqli($servername, $dbUserName, $dbPassword, $dbName);
 $conn->set_charset("utf8mb4");
 if ($conn->connect_error) {
